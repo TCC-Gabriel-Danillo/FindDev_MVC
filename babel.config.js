08e72b62a08f7d08d51c203765e77d90b9124901
GabrielDvPereira <gdavi.gd@gmail.com>
2022-12-07 20:49:26 -0300

    chore: setting up path configuration

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,26 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '_assets': './assets',
+            '_actions': './src/actions',
+            '_store': './src/store',
+            '_types': './src/types',
+            '_views': './src/views',
+            "_": "./src"
+          },
+          extensions: [
+            ".js",
+            ".jsx",
+            ".ts",
+            ".tsx",
+          ]
+        },
+      ]
+   ],
   };
 };
