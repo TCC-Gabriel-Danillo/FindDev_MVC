8f1793ba2c3506b81f3ba964cd27fac99d5ca1d1
GabrielDvPereira <gdavi.gd@gmail.com>
2022-12-11 13:14:16 -0300

    feat: configure extra arguments for store

--- src/store/config.ts
+++ src/store/config.ts
@@ -1,12 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { usersReducer } from "./usersStore"
 import { authReducer } from "./authStore"
+import {
+  HttpRepositoryImp,
+  DatabaseRepositoryImp
+} from '_/repositories'
+import { FIREBASE_COLLECTION, GITHUB_URL } from '_/constants'
+import { MiddlewareOptions } from '_/types'
 
 export const store = configureStore({
-    reducer: {
-      auth: authReducer,
-      users: usersReducer,
-    },
+  reducer: {
+    auth: authReducer,
+    users: usersReducer,
+  },
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware<MiddlewareOptions>({
+      thunk: {
+        extraArgument: {
+          gitHubApi: new HttpRepositoryImp(GITHUB_URL.API_BASE_URL),
+          gitHubAuthApi: new HttpRepositoryImp(GITHUB_URL.AUTH_BASE_URL),
+          userDatabaseRepository: new DatabaseRepositoryImp(FIREBASE_COLLECTION.USERS)
+        }
+      }
+    })
+  },
 })
 
 export type RootState = ReturnType<typeof store.getState>
