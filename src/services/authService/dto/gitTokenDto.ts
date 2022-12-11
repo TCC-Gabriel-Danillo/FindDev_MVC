b20fcf98b0b6dfea669dc7dd61b61cf98046478c
GabrielDvPereira <gdavi.gd@gmail.com>
2022-12-11 14:43:50 -0300

    feat: add git dto types

---
+++ src/services/authService/dto/gitTokenDto.ts
@@ -0,0 +1,3 @@
+export interface GitTokenDto {
+    access_token: string
+}
---
+++ src/services/authService/dto/gitUserDto.ts
@@ -0,0 +1,7 @@
+export interface GitUserDto {
+    login: string;
+    id: number;
+    avatar_url: string;
+    html_url: string;
+    email?: any;
+}
