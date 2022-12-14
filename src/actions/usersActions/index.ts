c317caabe6f94e3a2647f6017b82de1bdf50adec
GabrielDvPereira <gdavi.gd@gmail.com>
2022-12-14 15:50:56 -0300

    feat: add list users to user action

--- src/actions/usersActions/index.ts
+++ src/actions/usersActions/index.ts
@@ -1,21 +1,21 @@
 import { Dispatch } from "@reduxjs/toolkit"
-import { addUsers } from "_/store/usersStore"
-import { AppThunk } from "_/types"
+import { alertError } from "_/helpers";
+import { addUsers, usersLoading, usersLoaded } from "_/store/usersStore"
+import { AppThunk, LatLng } from "_/types"
 
-export const getUsersAction = (): AppThunk => {
-    return (dispatch: Dispatch, getState, { userService }) => {
-        dispatch(addUsers([
-            {
-                id: 100,
-                position: {
-                    latitude: 0,
-                    longitude: 0,
-                    geohash: "",
-                },
-                profileUrl: "profileUrl",
-                username: "username",
+const defaultDistanceInM = 10 * 1000;
 
-            }
-        ]))
+export const getUsersAction = (location: LatLng): AppThunk => {
+    return async (dispatch: Dispatch, getState, { userService }) => {
+        try {
+            dispatch(usersLoading())
+            const users = await userService.listUsersByDistance(location, defaultDistanceInM)
+            dispatch(addUsers(users))
+        } catch (err) {
+            console.error(err)
+            alertError("Algo deu errado ao recuperar usuários.")
+        } finally {
+            dispatch(usersLoaded())
+        }
     }
 }
