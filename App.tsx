8dc5212487509d64939731476f3e036fe4cc9a30
Gabriel Davi Silva Pereira <gdavi.gd@gmail.com>
2022-12-20 22:46:34 -0300

    Revert "Set up tests with redux (#6)" (#7)
    This reverts commit ef64c40ac6bc931dda6a818e5894ddd7f1acebaf.

--- App.tsx
+++ App.tsx
@@ -2,22 +2,12 @@ import "_/config/firebaseConfig"
 
 import { View } from "react-native"
 import { Provider } from 'react-redux'
-import { setupStore } from "_/store"
+import { store, persistedStore } from "_/store"
 import { Routes } from '_/view/navigation';
 import { useCustomFonts } from "_/hooks/useCustomFont";
 import { PersistGate } from 'redux-persist/integration/react'
-import { makeAuthService } from "_/helpers/makeAuthService";
-import { makeUserService } from "_/helpers";
-import { persistStore } from "redux-persist";
 
 
-
-const store = setupStore({
-  authService: makeAuthService(),
-  userService: makeUserService()
-})
-
-export const persistedStore = persistStore(store)
 
 export default function App() {
 
--- src/__tests__/auth/authtentication.test.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthScreen } from "_/view/screens"
-import { renderWithProviders } from "../utils/renderWithProvider"
-
-describe("Authentication", () => {
-    it("should render de auth screen", () => {
-        const { findByText } = renderWithProviders(<AuthScreen />)
-        findByText("Bem Vindo ao FindDev!")
-    })
-})
